c261bef471661d0ccfe3f3973b39d14c8d7da6f5
Johan Winther <johwin@student.chalmers.se>
2017-09-24 18:33:47 +0200

    ...

--- files/email.php
+++ files/email.php
@@ -1,11 +1,18 @@
 <?php
+echo getenv('email');
 use PHPlib\SMTP;
+echo getenv('email');
 $mail = new SMTP(require __DIR__ . 'email-config.php');
+echo getenv('email');
 $mail->to('welocy95@gmail.com');
 echo getenv('email');
 $mail->from(getenv('email'), 'Ebba Ekblom'); // email is required, name is optional
+echo getenv('email');
 $mail->reply('ekebba@student.chalmers.se', 'Ebba Ekblom');
+echo getenv('email');
 $mail->subject('Streckskuld');
+echo getenv('email');
 $mail->body('This is a <b>HTML</b> email.');
+echo getenv('email');
 $result = $mail->send();
-echo $result ?>
+echo $result; ?>
