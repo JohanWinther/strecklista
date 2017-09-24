860c9c47c01b397fa8b748e41b44ce2aa03f18f9
Johan Winther <johwin@student.chalmers.se>
2017-09-24 18:20:58 +0200

    New simple php mail lib

--- files/email.php
+++ files/email.php
@@ -1,43 +1,10 @@
 <?php
-echo "Hej";
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp-mail.outlook.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'ftek-streckning@outlook.com';                 // SMTP username
-    $mail->Password = '.mRM~!m,';                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
-
-    //Recipients
-    $mail->setFrom('ftek-streckning@outlook.com', 'Streckmail Boten');
-    $mail->addAddress('johan.winther@outlook.com', 'Wello');     // Add a recipient
-    $mail->addReplyTo('ekebba@student.chalmers.se', 'Ebba Ekblom');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
-
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Streckskuld';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-}
-?>
+use SMTP;
+$mail = new SMTP(require __DIR__ . 'email-config.php');
+$mail->to('welocy95@gmail.com');
+$mail->from(getenv('email'), 'Ebba Ekblom'); // email is required, name is optional
+$mail->reply('ekebba@student.chalmers.se', 'Ebba Ekblom');
+$mail->subject('Streckskuld');
+$mail->body('This is a <b>HTML</b> email.');
+$result = $mail->send();
+ ?>
