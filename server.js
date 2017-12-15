608572ef1b3b4d1dbca2ee69403529900700431b
Johan Winther <johan.winther@outlook.com>
2017-12-16 00:06:10 +0100

    New rules for linting

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 // Node.js notation for importing packages
-var express = require('express');
+var express = require("express")
 
 // Spin up a server
-var app = express();
+var app = express()
 
 // Get configurations for which directory to serve
 var config = {
-    "production" : {
-        buildPath: "/build/default"
-    },
-    "development": {
-        buildPath: ""
-    }
+  "production" : {
+    buildPath: "/build/default"
+  },
+  "development": {
+    buildPath: ""
+  }
 }
 
 // Serve static files from the main build directory
-app.use(express.static(__dirname + config[app.get('env')].buildPath));
+app.use(express.static(__dirname + config[app.get("env")].buildPath));
 
 // Render index.html on the main page, specify the root
-app.get('*', function(req, res){
-    res.sendFile("index.html", {root: '.'});
+app.get("*", function(req, res){
+  res.sendFile("index.html", {root: "."});
 });
 
 // set the port of our application
@@ -28,5 +28,5 @@ var port = process.env.PORT || 8080;
 
 // Tell the app to listen for requests on port 3000
 app.listen(port, function () {
-    console.log('App is up and running in ' + app.get('env') + ' mode!');
+  console.log("App is up and running in " + app.get("env") + " mode!");
 });
--- sw-precache-config.js
+++ sw-precache-config.js
@@ -12,17 +12,17 @@
 
 module.exports = {
   staticFileGlobs: [
-    'bower_components/webcomponentsjs/webcomponents-loader.js',
-    'images/*',
-    'manifest.json',
+    "client/bower_components/webcomponentsjs/webcomponents-loader.js",
+    "resources/images/*",
+    "manifest.json"
   ],
   runtimeCaching: [
     {
-      urlPattern: /\/bower_components\/webcomponentsjs\/.*.js/,
-      handler: 'fastest',
+      urlPattern: /client\/bower_components\/webcomponentsjs\/.*.js/,
+      handler: "fastest",
       options: {
         cache: {
-          name: 'webcomponentsjs-polyfills-cache',
+          name: "webcomponentsjs-polyfills-cache",
         },
       },
     },
